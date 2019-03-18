e6d79d89b4f5c6fde4ff08144a15b14faf06473f
uxff <uxff@qq.com>
2019-03-18 14:00:14 +0800

    update zoomer

--- apps/zoomer/zoomer.go
+++ apps/zoomer/zoomer.go
@@ -65,27 +65,25 @@ func (d *Dot) GetNW() []Dot {
 	return pos
 }
 
-func ForeachImgDot(imgPath string, walkHandler func(o image.Image, x int, y int, c color.Color)) error {
-	var imgIo, _ = os.Open(imgPath)
-	defer imgIo.Close()
+func ForeachImgDot(imgIo image.Image, walkHandler func(o image.Image, x int, y int, c color.Color)) error {
 
 	if walkHandler == nil {
 		return fmt.Errorf("walkHandler is NULL")
 	}
 
-	var pngIo, err = png.Decode(imgIo)
+	//var pngIo, err = png.Decode(imgIo)
 
-	if err != nil {
-		fmt.Println("png.decode err when read colorTpl:", err)
-		return err
-	}
+	//	if err != nil {
+	//		fmt.Println("png.decode err when read colorTpl:", err)
+	//		return err
+	//	}
 
-	width := pngIo.Bounds().Dx()
-	height := pngIo.Bounds().Dy()
+	width := imgIo.Bounds().Dx()
+	height := imgIo.Bounds().Dy()
 
 	for hi := 0; hi < height; hi++ {
 		for wi := 0; wi < width; wi++ {
-			walkHandler(pngIo, wi, hi, pngIo.At(wi, hi))
+			walkHandler(imgIo, wi, hi, imgIo.At(wi, hi))
 		}
 	}
 
@@ -110,11 +108,47 @@ func ImgToFile(img image.Image, outputFilePath string) {
 }
 
 func main() {
+
 	oldTopoPath := ""
 	newTopoPath := ""
 
-	o := image.NewRGBA(image.Rect(0, 0, 1024, 1024))
-	ForeachImgDot(oldTopoPath, WalkTopo)
-	ImgToFile(o, newTopoPath)
+	fOld, err := os.Open(oldTopoPath)
+	if err != nil {
+		fmt.Printf("open:%s failed:%v+\n", oldTopoPath, err)
+		return
+	}
+	defer fOld.Close()
+
+	imgOld, err := png.Decode(fOld)
+	if err != nil {
+		fmt.Printf("png.Decode:%s failed:%v+\n", oldTopoPath, err)
+		return
+	}
+
+	width, height := imgOld.Bounds().Dx(), imgOld.Bounds().Dy()
+
+	imgNew := image.NewRGBA(image.Rect(0, 0, width*2, height*2))
+
+	dotCounter := 0
+
+	ForeachImgDot(imgOld, func(o image.Image, x int, y int, c color.Color) {
+		dotCounter++
+		d := &Dot{x: x, y: y}
+		var dots []*Dot
+		// dotNE
+		dots = d.GetNE()
+		for _, do := range dots {
+			if do.x < 0 || do.y < 0 || do.x >= width || do.y >= height {
+				break
+			}
+		}
+		// dotSE
+		oldSEDots := getNeighbors()
+		// dotSW
+		// dotNW
+	})
+
+	ImgToFile(imgNew, newTopoPath)
+	fmt.Printf("dotCounter:%d\n", dotCounter)
 
 }
