a78a008d0033f7d9e70bf1aaa0ad1337d2f4b00d
uxff <uxff@qq.com>
2019-03-29 18:32:50 +0800

    add namer

---
+++ apps/namer/namer.go
@@ -0,0 +1,17 @@
+package main
+
+const As = `aeiou`
+var Ass = []string{
+	"a", "an", "ai", "ao", "au", "air", "are", "ang", "ay",
+	"e", "en", "ei", "eo", "eu", "eir", "ere", "eng", "ey", "ee",
+	"i", "in", "ia", "ie", "io", "iu", "ire", "ing", "ii",
+	"o"
+	}
+
+func MakeName() {
+
+}
+
+func main() {
+
+}
