d6a338a02dc6844d6d7bb278b0ba61274b89b1bf
uxff <uxff@qq.com>
2019-09-12 18:42:04 +0800

    update appv3

--- apps/appv3/topomaker.go
+++ apps/appv3/topomaker.go
@@ -189,17 +189,10 @@ func (d *Droplet) Move(m *Topomap, w *WaterMap, drops []*Droplet) {
 	}
 
 	d.vx, d.vy = d.vx+w.data[oldIdx].xPower, d.vy+w.data[oldIdx].yPower
+	w.data[oldIdx].q++ // 流出，才算流量
 
 	// 地形场加速
 	//d.vx, d.vy = (d.vx+w.data[oldIdx].xPower)/2, (d.vy+w.data[oldIdx].yPower)/2
-
-	// 没有场 可撒欢
-	if w.data[oldIdx].xPower == 0 && w.data[oldIdx].yPower == 0 {
-		// 自己生速度 比较浪
-		d.GenVeloByFallPower(m, w)
-		//log.Printf("no field power, after play self(x=%f,y=%f, vx,vy=%f,%f) fallPower=%d", d.x, d.y, d.vx, d.vy, d.fallPower)
-		//return
-	}
 
 	// 距离平方在2以内的WaterDot有吸引力
 	for _, di := range drops {
@@ -208,6 +201,14 @@ func (d *Droplet) Move(m *Topomap, w *WaterMap, drops []*Droplet) {
 			// di 是在范围sqrt(8)以内的水滴
 			d.CloseTo(di, distSquare) // 靠近
 		}
+	}
+
+	// 没有场 可撒欢
+	if w.data[oldIdx].xPower == 0 && w.data[oldIdx].yPower == 0 {
+		// 自己生速度 比较浪
+		d.GenVeloByFallPower(m, w)
+		//log.Printf("no field power, after play self(x=%f,y=%f, vx,vy=%f,%f) fallPower=%d", d.x, d.y, d.vx, d.vy, d.fallPower)
+		//return
 	}
 
 	// 将超出的速度限制成标准速度
@@ -229,7 +230,7 @@ func (d *Droplet) Move(m *Topomap, w *WaterMap, drops []*Droplet) {
 	// 无力场，待在原地
 	if newIdx == oldIdx {
 		//log.Printf("no field power. stay here.")
-		w.data[oldIdx].q++ // 流出，才算流量
+		//w.data[oldIdx].q++ // 流出，才算流量
 		return
 	}
 
@@ -240,7 +241,7 @@ func (d *Droplet) Move(m *Topomap, w *WaterMap, drops []*Droplet) {
 
 	// droplet no need lock
 	//d.x, d.y = tmpX, tmpY
-	d.hisway = append(d.hisway, newIdx)
+	//d.hisway = append(d.hisway, newIdx)
 	d.fallPower += int(m.data[oldIdx]-m.data[newIdx]) * 100
 
 	// 更新watermap前加锁
@@ -250,15 +251,11 @@ func (d *Droplet) Move(m *Topomap, w *WaterMap, drops []*Droplet) {
 
 	w.data[oldIdx].h--
 	w.data[newIdx].h++
-	w.data[oldIdx].q++ // 流出，才算流量
+	//w.data[oldIdx].q++ // 流出，才算流量
 }
 
-// 根据落差能量移动 包括位置浮动和速度浮动
+// 根据落差能量移动 包括位置浮动和速度浮动 只更改droplet
 func (d *Droplet) GenVeloByFallPower(m *Topomap, w *WaterMap) {
-	//mu := sync.Mutex{}
-	//mu.Lock()
-	//defer mu.Unlock()
-
 	if d.fallPower > 0 {
 		tmpRoll := rand.Float32()
 		// 小于一定的几率才执行方向浮动
@@ -271,14 +268,13 @@ func (d *Droplet) GenVeloByFallPower(m *Topomap, w *WaterMap) {
 
 		// 在一定几率下 位移浮动
 		if tmpRoll < 0.5 {
-			tmpDir := (rand.Float64() - rand.Float64()) * math.Pi * 2
-			fx, fy := float32(math.Cos(tmpDir)), float32(math.Sin(tmpDir))
+			//tmpDir := (rand.Float64() - rand.Float64()) * math.Pi * 2
+			fx, fy := rand.Float32()-tmpRoll, rand.Float32()-tmpRoll //float32(math.Cos(tmpDir)), float32(math.Sin(tmpDir))
 			d.x, d.y = d.x+fx/1.0, d.y+fy/1.0
 		}
 
 		//log.Printf("drop playself fx,fy=%f,%f", fx, fy)
 		d.fallPower--
-		return
 	}
 }
 
