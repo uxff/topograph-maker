daf74a75bbbd58cd120f0ec98ff5212fccd3c588
uxff <uxff@qq.com>
2020-10-08 17:01:17 +0800

    fix namer

--- apps/namer/namer.go
+++ apps/namer/namer.go
@@ -1,17 +1,84 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
 const As = `aeiou`
+
 var Ass = []string{
-	"a", "an", "ai", "ao", "au", "air", "are", "ang", "ay",
-	"e", "en", "ei", "eo", "eu", "eir", "ere", "eng", "ey", "ee",
-	"i", "in", "ia", "ie", "io", "iu", "ire", "ing", "ii",
-	"o", "oa", "oe", "oi", "oo", "ou", "on", "ong", "oan", "",
+	"a", "an", "ai", "ao", "au", "ar", "ang", "al",
+	"e", "en", "ei", "ee", "eo", "eu", "er", "eng", "el", "ea",
+	"i", "in", "ia", "ie", "io", "iu", "ir", "ing", "ii", "il", "ian", "iang",
+	"o", "oa", "oe", "oi", "oo", "ou", "on", "ong", "or", "ol",
+	"u", "ua", "ue", "ui", "uo", "uu", "un", "ung", "ur", "ul", "uan", "uang",
+	"iui", "iua", "iue", "iuo", "iun", "iuan", "iuang", "iuai", "iuao",
+}
+
+var Bss = []string{
+	"b", "p", "m", "f", "v", "w",
+	"d", "t", "n", "l",
+	"ds", "ts", "s", "z",
+	"j", "ch", "sh", "r",
+	// "ji", "chi", "shi", // j q x
+}
+
+func MakeName() string {
+	s := make([]string, 0)
+	alllen := 2 + rand.Intn(8)
+	for i := 0; i < alllen; i++ {
+		abRoller := rand.Float32()
+		if abRoller > 0.3 {
+			// ass
+			aRoller := rand.Intn(len(Ass))
+			s = append(s, Ass[aRoller])
+		} else {
+			// bss
+			bRoller := rand.Intn(len(Bss))
+			s = append(s, Bss[bRoller])
+		}
 	}
+	s1 := strings.Join(s, "")
+	s1 = strings.Trim(s1, " ")
+	if len(s1) > 0 && s1[0] > 96 {
+		//s1[0] = s1[0] - 32
+		s1 = strings.ToUpper(s1[:1]) + s1[1:]
+	}
+	return s1
+}
 
-func MakeName() {
+var Acc = "aeiou "
+var Bcc = "qwrtypsdfghjklzxcvbnm"
 
+// 此方法来自VB
+func MakeName2() []byte {
+	s1 := make([]byte, 0)
+	rnum := rand.Intn(6) + 3 // 循环2到7次,产生4到14个随机字母
+	for i := 1; i < rnum; i++ {
+		rand.Seed(time.Now().UnixNano() + int64(i))
+		if rand.Intn(5) > 2 { // 随机置换声母和韵母的位置
+			//
+			s1 = append(s1, byte(rand.Intn(26)+97), Acc[rand.Intn(len(Acc))])
+		} else { // 随机取一个声母和一个韵母
+			//
+			s1 = append(s1, Acc[rand.Intn(len(Acc))], byte(rand.Intn(26)+97))
+		}
+	}
+	s1 = bytes.Trim(s1, " ")
+	if len(s1) > 0 && s1[0] > 96 {
+		s1[0] = s1[0] - 32
+	}
+	return s1
 }
 
 func main() {
-
+	rand.Seed(time.Now().UnixNano())
+	s1 := MakeName()
+	fmt.Printf("after make, s=%s\n", s1)
+	s2 := MakeName2()
+	fmt.Printf("after make, s=%s\n", s2)
 }
